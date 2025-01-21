136f4ef2c13766638895dbb5f09f01d660038c30
esoundarya85 <esoundarya85@gmail.com>
2025-01-21 09:57:08 -0800

    second change

--- BaseClass.java
+++ BaseClass.java
@@ -31,7 +31,7 @@ public class BaseClass {
 
 	public void setup() throws Exception {
 		if (driver == null) {
-			System.out.println("driver is not initialized yet");
+			System.out.println("driver is not initialized yet for the code");
 			String directory = System.getProperty("user.dir");
 			System.out.println("The project part is " + directory);
 
